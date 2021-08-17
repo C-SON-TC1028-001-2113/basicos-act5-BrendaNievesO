633c5c0b88799cecaaf0d3d181652d4343b041aa
BrendaNievesO <a01252478@itesm.mx>
2021-08-17 22:45:02 +0000

    actClase5

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,6 +1,10 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
+    area=float(input('Area de la pared: '))
+    redim=float(input('Redimiento (m2/l): '))
+    litros=math.ceil(area/redim)
+    print('La cantidad necesaria de litros a comprar: '+str(litros))
 
 
 if __name__ == '__main__':
